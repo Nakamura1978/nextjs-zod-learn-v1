8225cd03101a617cefd0f4a273a63c353d971038
Nakamura1978 <125366330+Nakamura1978@users.noreply.github.com>
2023-10-23 21:05:49 +0900

    ✨ 登録用ＡＰＩの作成

--- src/app/api/user/route.tsx
+++ src/app/api/user/route.tsx
@@ -13,3 +13,19 @@ export const GET = async (req: Request, res: NextResponse) => {
     await prisma.$disconnect();
   }
 };
+
+//ユーザー登録用のＡＰＩ
+export const POST = async (req: Request, res: NextResponse) => {
+  try {
+    await main();
+    const { name, postNum, mail, prefecture, password } = await req.json();
+    const usr = await prisma.usr.create({
+      data: { name, postNum, mail, prefecture, password },
+    });
+    return NextResponse.json({ message: "Success", usr }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error" }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
