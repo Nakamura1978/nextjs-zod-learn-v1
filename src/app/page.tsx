b459e22de5b93e38bfbac6f4093f7653fa7d17e3
Nakamura1978 <125366330+Nakamura1978@users.noreply.github.com>
2023-10-24 20:28:56 +0900

    🐛 再々修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,84 @@
-import Image from "next/image";
+"use client";
+
+import { useRef } from "react";
 
 export default function Home() {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const postNumRef = useRef<HTMLInputElement>(null);
+  const mailRef = useRef<HTMLInputElement>(null);
+  const prefectureRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
   return (
     <main>
-      <h1>登録フォーム</h1>
-      <form></form>
+      <h1 className="text-center text-xl my-10">登録フォーム</h1>
+      <form className="w-3/5 mx-auto text-lg flex flex-col space-y-6">
+        <div className="flex justify-between items-center">
+          <label htmlFor="name" className="w-1/4 text-center">
+            名前
+          </label>
+          <input
+            ref={nameRef}
+            type="text"
+            id="name"
+            name="name"
+            placeholder="中村　淳"
+            className="bg-gray-50 rounded-md border-2 shadow-md px-3 py-1 w-3/4"
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label htmlFor="name" className="w-1/4 text-center">
+            名前
+          </label>
+          <input
+            ref={nameRef}
+            type="text"
+            id="name"
+            name="name"
+            placeholder="中村　淳"
+            className="bg-gray-50 rounded-md border-2 shadow-md px-3 py-1 w-3/4"
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label htmlFor="name" className="w-1/4 text-center">
+            名前
+          </label>
+          <input
+            ref={nameRef}
+            type="text"
+            id="name"
+            name="name"
+            placeholder="中村　淳"
+            className="bg-gray-50 rounded-md border-2 shadow-md px-3 py-1 w-3/4"
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label htmlFor="name" className="w-1/4 text-center">
+            名前
+          </label>
+          <input
+            ref={nameRef}
+            type="text"
+            id="name"
+            name="name"
+            placeholder="中村　淳"
+            className="bg-gray-50 rounded-md border-2 shadow-md px-3 py-1 w-3/4"
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label htmlFor="name" className="w-1/4 text-center">
+            名前
+          </label>
+          <input
+            ref={nameRef}
+            type="text"
+            id="name"
+            name="name"
+            placeholder="中村　淳"
+            className="bg-gray-50 rounded-md border-2 shadow-md px-3 py-1 w-3/4"
+          />
+        </div>
+      </form>
     </main>
   );
 }
